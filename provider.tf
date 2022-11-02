9b03b6c0c011046117e0b36f261d5af4faf95124
Chad Bates <cbates255@gmail.com>
2022-11-02 14:20:24 -0400

    providers

--- provider.tf
+++ provider.tf
@@ -32,7 +32,9 @@ provider "kubernetes" {
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 resource "random_string" "suffix" {
